fd1d61bbc924f2b0c9fc4c33e540fe24248c8c6d
henriqueparpinelli <henrique.parpinelli@escola.pr.gov.br>
2025-07-31 08:46:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function multiplicaçao(){
+    let  = juros_simples = document.parseInt.getElementById('juros_simples').value;
+
+    let capital_inicial = document.parseInt.getElementById('capital_inicial').value;
+
+    let porcentagem_de_juros = document.parseInt.getElementById('porcentagem_de_juros').value;
+
+    let tempo = document.parseInt.getElementById('tempo').value;
+
+    let multiplicaçao_juros_simples = capital_inicial*porcentagem_de_juros*tempo
+    alert("juros_simples")
+    calculo_de_juros_simples.getElementById('resultado').textContent = 'resultado' + multiplicaçao_juros_simples;
+
+    
+}
